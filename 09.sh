20b75603dfa62c7aae089093cf6cb11f904bfc28
Jayanth <jayanth6777@gmail.com>
2024-04-19 23:04:28 -0400

    Second

--- 09.sh
+++ 09.sh
@@ -5,6 +5,10 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 VALIDATE(){
     if [ $1 -ne 0]
     then
@@ -31,11 +35,11 @@ do
   dnf list installed $i &>>$LOGFILE
   if [ $? -eq 0 ]
   then
-      echo "$i already installed"
+      echo "$i already installed....$R Skipped $N"
   else
       echo "$i not installed"
   fi
-  
+
 done
 
 
